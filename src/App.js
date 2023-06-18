15b50b56fec5f085101133dfe7dcb01c242f944f
seyhunDev <136964579+seyhunDev@users.noreply.github.com>
2023-06-18 17:50:02 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
       <div className="App">
         <Navbar />
         <Routes>
-          <Route path="/home" element={< Home />} />
+          <Route path="/" element={< Home />} />
           <Route path="/about" element={< About />} />
           <Route path="/menuone" element={< Menuone/>} />
          
